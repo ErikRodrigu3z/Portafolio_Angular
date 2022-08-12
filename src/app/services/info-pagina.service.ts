cff62da8b2679f15051cf7c88b68448ef0cb6ac1
earg <earg@citlali.mx>
2022-08-11 18:01:08 -0600

    se agrega firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   loaded = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) { 
-    
+    this.CargarIfo();
+    this.CargarEquipo();   
+
+  }
+
+  private CargarIfo(){
     this.http.get('assets/data/data-page.json')
     .subscribe( (resp: InfoPagina) => {
       this.loaded = true;
       this.info = resp;
+    });
+  }
 
-      // console.log("Pagina cargada");
+  private CargarEquipo(){
+    this.http.get('https://portafolio-angular-1a07a-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
       // console.log(resp);
-      
     });
 
   }
+
+
 }
